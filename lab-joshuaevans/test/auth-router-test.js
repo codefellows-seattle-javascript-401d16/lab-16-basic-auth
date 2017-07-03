356e8d5021b40f5708d4be51f8a3c23a3a2bf749
Joshua Evans <skyfriends1@gmail.com>
2017-07-03 14:41:45 -0700

    finished writing out server/routes/tests/model

---
+++ lab-joshuaevans/test/auth-router-test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+require('dotenv').config({path: `${__dirname}/../.test.env`});
+
+const expect = require('expect');
+const superagent = require('superagent');
+const server = require('../lib/server.js');
+const cleanDB = require('./lib/clean-db.js');
+const mockUser = require('./lib/mock-user.js');
+
+const API_URL = process.env.API_URL;
+
+describe('testing auth router', () => {
+  before(server.start);
+  after(server.stop);
+  afterEach(cleanDB);
+
+  describe('testing POST /api/signup', () => {
+    it('should respond with a token', () => {
+      return superagent.post(`${API_URL}/api/signup`)
+      .send({
+        username: 'test_user',
+        password: 'top secret',
+        email: 'test@test.com',
+      })
+      .then(res => {
+        console.log('token we got back', res.body);
+        expect(res.status).toEqual(200);
+        expect(res.text).toExist;
+        expect(res.text.length > 1).toBeTruthy();
+      });
+    });
+    it('should return 400 for bad request if body is missing or invalid', () => {
+      return superagent.post(`${API_URL}/api/signup`)
+      .catch(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual(undefined);
+      });
+    });
+  });
+
+  describe('testing GET /api/login', () => {
+    it('should respond with a token', () => {
+      let tempUser;
+      return mockUser.createOne()
+      .then(userData => {
+        tempUser = userData.user;
+        let encoded = new Buffer(`${tempUser.username}:${userData.password}`).toString('base64');
+        return superagent.get(`${API_URL}/api/login`)
+        .set('Authorization', `Basic ${encoded}`);
+      })
+      .then(res => {
+        expect(res.status).toEqual(200);
+        expect(res.text).toExist();
+        expect(res.text.length > 1).toBeTruthy();
+      });
+    });
+    it('should return 404 if route has not been registered', () => {
+      return superagent.get(`${API_URL}/api/notarealroute`)
+      .catch(res => {
+        expect(res.status).toEqual(404);
+      });
+    });
+    it('should respond with a 401 if the user is not authenticated', () => {
+      let tempUser;
+      return mockUser.createOne()
+      .then(userData => {
+        tempUser = userData.user;
+        let encoded = new Buffer(`${tempUser.username}:${userData.password}`);
+        return superagent.get(`${API_URL}/api/login`)
+        .set('Authorization', `Basic ${encoded}`);
+      })
+      .catch(res => {
+        expect(res.status).toEqual(401);
+      });
+    });
+  });
+});
