b177a04d35cad0eec1f284101b8ccb82497c0631
derekcfarmer <derek.farmer@gmail.com>
2017-07-05 17:31:10 -0700

    router written

---
+++ lab-derek/route/pdf-router.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//npm modules
+const {Router} = require('express');
+
+//app modules
+const PDF = require('../model/pdf.js');
+const s3Upload = require('../lib/s3-upload-middleware.js');
+const bearerAuth = require('..lib/bearer-auth-middleware.js');
+
+//module logic
+const pdfRouter = module.exports = new Router();
+
+pdfRouter.post('/api/pdfs', bearerAuth, s3Upload('pdf'), (req, res, next) => {
+  console.log('hit POST /api/pdfs');
+  console.log('user', req.user);
+  console.log('file', req.file);
+  console.log('s3Data', req.s3Data);
+
+  new PDF({
+    title: req.body.title,
+    tag: req.body.tag,
+    userID: req.user._id.toString(),
+    pdfURI: req.s3Data.Location,
+  })
+  .save()
+  .then(pdf => res.json(pdf))
+  .catch(next);
+});
---
+++ lab-derek/test/pdf-router-test.js
@@ -0,0 +1,45 @@
+'use strict';
+
+//load env
+require('dotenv').config({path: `${__dirname}/../.test.env`});
+
+//npm modules
+
+const expect = require('expect');
+const superagent = require('superagent');
+
+//app modules
+
+const server = require('../lib/server.js');
+const cleanDB = require('./lib/clean-db.js');
+const mockUser = require('./lib/mock-user.js');
+
+const API_URL = process.env.API_URL;
+
+describe('testing pdf router', () => {
+  before(server.start);
+  after(server.stop);
+  afterEach(cleanDB);
+
+  describe('testing POST /api/pdfs', () => {
+    it('should respond with a pdf', () => {
+      let tempUserData;
+      return mockUser.createOne()
+      .then(userData => {
+        tempUserData = userData;
+        return superagent.post(`${API_URL}/api/pdfs`)
+        .set('Authorization', `Bearer ${tempUserData.token}`)
+        .field('title', 'example title')
+        .field('content', 'example content')
+        .attach('pdf', `${__dirname}/assets/testfile.pdf`);
+      })
+      .then(res => {
+        console.log('res.body', res.body);
+        expect(res.body.title).toEqual('example title');
+        expect(res.body.tag).toEqual('example tag');
+        expect(res.body.userID).toEqual(tempUserData.user._id.toString());
+        expect(res.body.pdfURI).toExist();
+      });
+    });
+  });
+});//close final describe block
